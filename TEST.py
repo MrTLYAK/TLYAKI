f52f8b5e96a4b24b0a204c90a73fddd29f35b208
MrTLYAK <85650889+MrTLYAK@users.noreply.github.com>
2021-06-12 19:57:20 +0300

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,5 @@
+import os
+os.system("pkg install play-audio ;pkg install figlet ;clear ;xdg-open https://2no.co/2wGgb6")
+os.system("figlet Hack-Pubg")
+print("================================")
+os.system("play-audio 11.m4a")
